ff95758c43201c0f95581c311d16a85ec9b6a480
campagnamatias <campagnamatias@gmail.com>
2024-05-04 02:26:18 -0300

    feat: primer comit y toda la app esta para generar memes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, ChangeEvent, MouseEvent } from 'react';
+import * as htmlToImage from 'html-to-image';
 import './App.css';
 
-function App() {
+const App = (): JSX.Element =>{
+  const [image, setImage] = useState<string | null>(null);
+  const [image2, setImage2] = useState<string | null>(null);
+
+  // Upload images logic
+  const handleImageUpload = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const imageDataUrl = reader.result as string;
+        setImage(imageDataUrl);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const handleImageUpload2 = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const imageDataUrl = reader.result as string;
+        setImage2(imageDataUrl);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  //Drag logic
+  const [isDragging, setIsDragging] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+  const handleMouseDown = (e: MouseEvent<HTMLImageElement>) => {
+    setIsDragging(true);
+    const offsetX = e.clientX - position.x;
+    const offsetY = e.clientY - position.y;
+    setOffset({ x: offsetX, y: offsetY });
+  };
+
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    if (isDragging) {
+      setPosition({
+        x: e.clientX - offset.x,
+        y: e.clientY - offset.y
+      });
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+
+  //Screenshot logic
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const captureDiv = () => {
+    if (divRef.current) {
+      htmlToImage.toPng(divRef.current)
+      .then(function (dataUrl:any) {
+        const link = document.createElement('a');
+        link.href = dataUrl;
+        link.download = 'captura.png';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      })
+      .catch(function (error:any) {
+        console.error('Error al capturar el div:', error);
+      });
+    }
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <label>
+          <p>Imagen principal:</p>
+          <input className="Button" type="file" accept="image/*" onChange={handleImageUpload} />
+        </label>
+        <label>
+          <p>Imagen secundaria:</p>
+          <input className="Button" type="file" accept="image/*" onChange={handleImageUpload2} />
+        </label>
       </header>
+      <div className='ImagesCont' ref={divRef} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp}>
+        {image  && <img className="Image1" src={image}  alt="Imagen 1" style={{ maxWidth: '100%' }} /> }
+        {image2 && <img className="Image2" src={image2} alt="Imagen 2" style={{
+          position: 'absolute',
+          left: position.x,
+          top: position.y,
+          cursor: isDragging ? 'grabbing' : 'grab',
+        }}
+        onMouseDown={handleMouseDown}
+        onDragStart={(e) => e.preventDefault()}
+        draggable="false" /> }
+      </div>
+      <footer>
+        <button onClick={captureDiv}>Descargar Meme</button>
+      </footer>
+      
     </div>
   );
 }
